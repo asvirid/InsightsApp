61fbb57b3db0a48127a3b84d9512eec892c6b8c0
asvirid <azuzulele@gmail.com>
2024-07-04 14:03:27 -0400

    Standardizing columns

--- insightsV1.py
+++ insightsV1.py
@@ -5,11 +5,11 @@ from tabulate import tabulate
 
 
 max_col_widths = {
-    'Transaction Date': 10,
-    'Category': 20,
-    'Debit': 15,
-    'Description': 35,
-    'Card No.': 7
+    'Transaction Date': {'min': 2, 'max': 3},
+    'Category': {'min': 44, 'max': 45},
+    'Debit': {'min': 5, 'max': 15},
+    'Description': {'min': 9, 'max': 10},
+    'Card No.': {'min': 5, 'max': 7}
 }
 
 def parse_dates(dates):
@@ -58,11 +58,11 @@ def read_files(dir):
             print(f"Error reading {filepath}: {e}")
     return dataframes
 
-def truncate_string(s, max_len):
+def truncate_string(s, max_len, min_len):
     s = str(s)
-    return s.ljust(30) if len(s) <= max_len else s[:max_len-3] + '...'
+    return s.ljust(min_len) if len(s) <= max_len else s[:max_len-3] + '...'
 
-def print_table(df, title=None, sum=None, max_width=None):
+def print_table(df, title=None, sum=None, max_col_widths=None):
     if title:
         print(f"\n{title}\n")
 
@@ -70,11 +70,12 @@ def print_table(df, title=None, sum=None, max_width=None):
         sum_row = pd.DataFrame([sum], index=["Summary"])
         df = pd.concat([df, sum_row])
 
-    if max_width:
+    if max_col_widths:
         for col in df.columns:
-            max_len=max_width.get(col)
-            if max_len:
-                df[col] = df[col].apply(lambda x: truncate_string(str(x), max_len))
+            max_len = max_col_widths.get(col, {}).get('max')
+            min_len = max_col_widths.get(col, {}).get('min')
+            if max_len and min_len:
+                df[col] = df[col].apply(lambda x: truncate_string(x, max_len, min_len))
     print(tabulate(df, headers='keys', tablefmt='rounded_outline', showindex=False))
 
 def group_and_summarize(df, group_by_col, sum_col):
@@ -102,12 +103,12 @@ def main():
         sorted['Category'] = sorted['Category'].fillna('Uncategorized')
         
         grouped_sum, grouped_entries = group_and_summarize(sorted, 'Category', 'Debit')
-        print_table(grouped_sum, title="Grouped Summary by Category", max_width=max_col_widths)
+        print_table(grouped_sum, title="Grouped Summary by Category", max_col_widths=max_col_widths)
 
         for cat, group_df in grouped_entries:
             cat_sum = group_df['Debit'].sum().round(2)
             summary_stats = {'Debit': f"Sum: {cat_sum}"}
-            print_table(group_df,title=f"Entries for Category: {cat}", sum=summary_stats, max_width=max_col_widths)
+            print_table(group_df,title=f"Entries for Category: {cat}", sum=summary_stats, max_col_widths=max_col_widths)
 
         sorted = sorted.sort_values(by='Debit', ascending = False)
         spent = sorted['Debit'].sum()
